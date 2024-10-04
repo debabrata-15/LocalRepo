ab30f66662a4061c0a1042b13ab7ffa64d77eb0f
debarata-15 <debabrata15112001@gmail.com>
2024-10-04 11:01:14 +0530

    Add inital files

---
+++ new.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(){
+    printf("This is a new repository");
+    return 0;
+}
---
+++ sum.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main(){
+    int a = 4, b = 5;
+    printf("The sum of the numbers is %d",a+b);
+    return 0;
+}
